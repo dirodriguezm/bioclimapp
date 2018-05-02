6ecc7c41c66c399ebf9a8ddda5c30a9c816b236d
dirodriguezm <drodriguezm@udec.cl>
2018-05-02 14:26:07 -0300

    componente de mapa

--- resources/assets/js/components/Example.js
+++ resources/assets/js/components/Example.js
@@ -1,79 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import MapContainer from './MapContainer';
 
 export default class Example extends Component {
-<<<<<<< HEAD
-  constructor() {
-    super();
-    //Initialize the state in the constructor
-    this.state = {
-        materiales: [],
-    }
+  constructor(props) {
+    super(props);
   }
-=======
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
->>>>>>> cb5305db21eb9df4abceeb95a571828eef627ad0
 
   /*componentDidMount() is a lifecycle method
    * that gets called after the component is rendered
    */
   componentDidMount() {
-    /* fetch API in action */
-    fetch('/api/materiales')
-        .then(response => {
-            return response.json();
-        })
-        .then(materiales => {
-            //Fetched product is stored in the state
-            this.setState({ materiales });
-        });
+
   }
-
-  renderProducts() {
-    return this.state.materiales.map(material => {
-        return (
-            /* When using list you need to specify a key
-             * attribute that is unique for each list item
-            */
-            <li key={material.id} >
-                { material.nombre }
-            </li>
-        );
-    })
-  }
-
-
 
   render() {
-    const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-      <GoogleMap
-        defaultZoom={15}
-        defaultCenter={{ lat: -36.82013519999999, lng: -73.0443904 }}
-      >
-        {props.isMarkerShown && <Marker position={{ lat: -36.82013519999999, lng: -73.0443904 }} />}
-      </GoogleMap>
-    ))
+
       return (
           <div className="container">
               <div className="row justify-content-center">
                   <div className="col-md-8">
                       <div className="card">
-                          <div className="card-header">Lista de materiales</div>
+                          <div className="card-header">Mapa</div>
+                          <div className="card-content">
+                            <MapContainer apiKey='AIzaSyAA_ayr9aMRSAPIugacjp_CL6S5ux_N4is'/>
+                          </div>
                       </div>
-                      <MyMapComponent
-                        isMarkerShown
-                        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-                        loadingElement={<div style={{ height: `100%` }} />}
-                        containerElement={<div style={{ height: `400px` }} />}
-                        mapElement={<div style={{ height: `100%` }} />}
-                      />
                   </div>
               </div>
           </div>
---
+++ resources/assets/js/components/MapContainer.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react';
+import {Map, GoogleApiWrapper} from 'google-maps-react';
+var SunCalc = require('suncalc');
+
+
+export class MapContainer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentLocation: {
+        lat: '-36.82013519999999',
+        lng: '-73.0443904',
+        elevation: -1
+      }
+    }
+    this.mapClicked = this.mapClicked.bind(this);
+    this.onDragend = this.onDragend.bind(this);
+    this.onMapReady = this.onMapReady.bind(this);
+  }
+
+  getGeocode(latLng, _callback){
+    var geocoder = new google.maps.Geocoder;
+    var res = "hola";
+    geocoder.geocode({'location': latLng}, function(results, status){
+      if (status === 'OK'){
+        if (results[1]){
+          _callback(results);
+        }
+      }
+    });
+  }
+
+  getElevationOfPoint(latLng, _callback){
+    var elevator = new google.maps.ElevationService;
+    elevator.getElevationForLocations({
+      locations: [latLng]
+    }, function(results, status){
+      if (status === 'OK'){
+        if (results[0]){
+          _callback(results[0]);
+        }
+      }
+    });
+  }
+
+  getSunPosition(latLng){
+    let sun = SunCalc.getPosition(/*Date*/ new Date(), /*Number*/ latLng.lat(), /*Number*/ latLng.lng());
+    let altitude = sun.altitude * 180 / Math.PI;
+    let azimuth = sun.azimuth * 180 / Math.PI;
+    return {
+      altitude: altitude,
+      azimuth: azimuth
+    }
+  }
+
+  createMarker(latLng, map, info_content){
+    return new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+
+  createInfoWindow(map, marker, info_content){
+    var infowindow = new google.maps.InfoWindow;
+    infowindow.setContent(info_content);
+    infowindow.open(map, marker);
+  }
+
+
+  mapClicked(mapProps, map, clickEvent){
+    this.getElevationOfPoint(clickEvent.latLng, (result) => {
+      this.setState({
+        currentLocation: {
+          lat: clickEvent.latLng.lat(),
+          lng: clickEvent.latLng.lng(),
+          elevation: result.elevation
+        }
+      });
+    });
+
+    let marker = this.createMarker(clickEvent.latLng,map);
+    this.getGeocode(clickEvent.latLng, (results) => {
+      this.createInfoWindow(map,marker,results[1].formatted_address);
+    });
+    //
+  }
+
+  onDragend(mapProps, map){
+    this.setState({
+      currentLocation: {
+        lat: map.getCenter().lat(),
+        lng: map.getCenter().lng()
+      }
+    });
+  }
+
+  onMapReady(mapProps, map){
+    const google = this.props.google;
+
+    if (!google || !map) return;
+
+    const autocomplete = new google.maps.places.Autocomplete(this.autocomplete);
+    autocomplete.bindTo('bounds', map);
+
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+
+      if (!place.geometry) return;
+
+      if (place.geometry.viewport) map.fitBounds(place.geometry.viewport);
+      else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+
+      this.setState({ currentLocation: place.geometry.location });
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    const style = {
+      width: '100vw',
+      height: '50vh'
+    }
+
+    return (
+      <div>
+        <div>
+          <form onSubmit={this.onSubmit}>
+            <input
+              placeholder="Ingrese localidad"
+              ref={ref => (this.autocomplete = ref)}
+              type="text"
+            />
+
+            <input className='button' type="submit" value="Go" type='hidden'  />
+          </form>
+        </div>
+        <div style={style}>
+         <Map className="map"
+            google={this.props.google}
+            onClick={this.mapClicked}
+            onDragend={this.onDragend}
+            onReady={this.onMapReady}
+            initialCenter={this.state.currentLocation}
+            center={this.state.currentLocation}
+         />
+       </div>
+     </div>
+    )
+  }
+}
+
+export default GoogleApiWrapper(
+  (props) => ({
+    apiKey: props.apiKey,
+  }
+))(MapContainer)
