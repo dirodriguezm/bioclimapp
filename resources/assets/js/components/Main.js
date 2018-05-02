fff22712c08d8f6790437d711bd46f7f89a1bbd3
dirodriguezm <drodriguezm@udec.cl>
2018-05-02 15:20:23 -0300

    scene con mapcontainer

--- resources/assets/js/components/Main.js
+++ resources/assets/js/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MapContainer from './MapContainer';
+import Scene from './World';
 
 export default class Main extends Component {
   constructor(props) {
@@ -19,14 +20,8 @@ export default class Main extends Component {
       return (
           <div className="container">
               <div className="row justify-content-center">
-                  <div className="col-md-8">
-                      <div className="card">
-                          <div className="card-header">Mapa</div>
-                          <div className="card-content">
-                            <MapContainer apiKey='AIzaSyAA_ayr9aMRSAPIugacjp_CL6S5ux_N4is'/>
-                          </div>
-                      </div>
-                  </div>
+                      <Scene/>
+                      <MapContainer apiKey='AIzaSyAA_ayr9aMRSAPIugacjp_CL6S5ux_N4is'/>
               </div>
           </div>
       );
